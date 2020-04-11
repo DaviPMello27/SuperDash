94ab554c69eba7b6246723230932829537060ced
DaviPMello27 <davipmello@edu.univali.br>
2020-04-11 01:50:17 -0300

    Added Map Reading and Movement
     + Map built from 20x20 array;
    
     + Movement and collisions;
    
     = Organized Player struct functions.

--- include/structs/player.h
+++ include/structs/player.h
@@ -3,6 +3,7 @@
 
 #include <SDL/SDL.h>
 #include "headers.h"
+#include "constants/map.h" //temporary until we build the maps using the Map struct
 
 struct DirectionKeys {
 	bool left;
@@ -25,15 +26,104 @@ enum State {
 	STATE_DEFEATED,
 };
 
+struct Speed {
+	float x;
+	float y;
+};
+
 struct Player {
 	Character* character;
 	Direction direction;
+	Speed speed;
 	State state;
 	DirectionKeys keys;
 	SDL_Point pos;
 	int dashRecovery;
 	int health;
 	int team;
+
+	void move(){ //we'll fix the float to int conversions later
+		float maxSpeed = 4; //temporary until we set maxSpeed based on character data
+		if(state == STATE_MIDAIR){
+			maxSpeed /= 1.5;
+		}
+		//jump
+		if(keys.up){
+			if(state == STATE_WALKING){
+				speed.y = -15;
+				pos.y--;
+				state = STATE_MIDAIR;
+			}
+		}
+
+		//collision mid air
+		if(state == STATE_MIDAIR){
+			speed.y += 1.0 / 3;
+
+			pos.y += speed.y;
+
+			//up collision detection
+			if(
+				speed.y < 0 &&                                               //if character is moving up
+				(map1[(pos.y) / 30][(pos.x) / 40] == 1 ||                    //if top-left is hitting block
+					map1[(pos.y) / 30][(pos.x + 19) / 40] == 1)              //if top-right is hitting block
+				){
+				pos.y += 30 - (pos.y) % 30; //set position to the bottom of the block
+				speed.y = 0;
+			}
+
+			if(map1[(pos.y + 0) / 30][(pos.x) / 40] == 1 ||                  //top left
+				map1[(pos.y + 0) / 30][(pos.x + 19) / 40] == 1 ||            //top right
+				map1[(pos.y + 25) / 30][(pos.x) / 40] == 1 ||                //middle left
+				map1[(pos.y + 25) / 30][(pos.x + 19) / 40] == 1 ||           //middle right
+				map1[(pos.y + 50) / 30][(pos.x) / 40] == 1 ||                //bottom left
+				map1[(pos.y + 50) / 30][(pos.x + 19) / 40] == 1){            //bottom right
+				pos.y -= (pos.y + 50) % 30; //set position to the top of the block
+				speed.y = 0;
+				state = STATE_WALKING;
+			}
+		}
+
+		if(keys.right){
+			//speed increase
+			if(speed.x < maxSpeed){
+				speed.x++;
+			}
+			pos.x += speed.x;
+			//right collision detection
+			if(map1[pos.y / 30][(pos.x + 19) / 40] == 1 ||                   //top right  
+				map1[(pos.y + 25) / 30][(pos.x + 19) / 40] == 1 ||           //middle right
+				map1[(pos.y + 49) / 30][(pos.x + 19) / 40] == 1){            //bottom right
+				pos.x -= speed.x;
+				speed.x = 0;
+			}
+		} else if(speed.x > 0){
+			speed.x--;
+		}
+
+		if(keys.left){
+			//speed increase
+			if(speed.x > -maxSpeed){
+				speed.x--;
+			}
+			pos.x += speed.x;
+			//left collision detection
+			if(map1[pos.y / 30][(pos.x) / 40] == 1 ||                        //top left
+				map1[(pos.y + 25) / 30][(pos.x) / 40] == 1 ||                //middle left
+				map1[(pos.y + 49) / 30][(pos.x) / 40] == 1){                 //bottom left
+				pos.x -= speed.x;
+				speed.x = 0;
+			}
+		} else if(speed.x < 0){
+			speed.x++;
+		}
+
+		if(state == STATE_WALKING &&
+			map1[(pos.y + 51) / 30][(pos.x) / 40] == 0 &&                    //bottom left
+			map1[(pos.y + 51) / 30][(pos.x + 20) / 40] == 0){                //bottom right
+			state = STATE_MIDAIR;
+		}
+	}
 };
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -11,18 +11,19 @@
 #include <iostream>
 #include <string>
 #include <SDL/SDL.h>
+#include <SDL/SDL_image.h>
 #include "structs/headers.h"
 #include "constants/constants.h"
 #include "globals/globals.h"
 
-Screen setWindowSize(int w, int h){
+Screen setWindowSize(int w, int h){ //TODO: put this in the Screen struct
 	return {
 		{w, h},
 		{w/800, h/800}
 	};
 }
 
-void eventHandler(){
+void eventHandler(Player &player){ //TODO: get this out of main.cpp
 	SDL_Event event;
 	while(SDL_PollEvent(&event)){
 		switch(event.type){
@@ -30,13 +31,51 @@ void eventHandler(){
 			dashSystem.running = false;
 			break;
 		}
+		switch(event.type){
+		case SDL_KEYDOWN:
+			switch(event.key.keysym.sym){
+			case SDLK_d:
+				player.keys.right = true;
+				break;
+			case SDLK_a:
+				player.keys.left = true;
+				break;
+			case SDLK_w:
+				player.keys.up = true;
+				break;
+			}
+			break;
+		case SDL_KEYUP:
+			switch(event.key.keysym.sym){
+			case SDLK_d:
+				player.keys.right = false;
+				break;
+			case SDLK_a:
+				player.keys.left = false;
+				break;
+			case SDLK_w:
+				player.keys.up = false;
+				break;
+			}
+			break;
+		}
 	}
 }
 
-int main(){
-	Screen screen = setWindowSize(800, 600); //TODO: implement file reading
-	SDL_Init(SDL_INIT_EVERYTHING);
-	SDL_Window* mainWindow = SDL_CreateWindow(
+
+void buildMap(SDL_Renderer* renderer, SDL_Texture* texture){ //TODO: put this in Map struct
+	for(int y = 0; y < 600; y += 30){
+		for(int x = 0; x < 800; x += 40){
+			if(map1[y / 30][x / 40] == 1){
+				SDL_Rect brick = {x, y, 40, 30};
+				SDL_RenderCopy(renderer, texture, nullptr, &brick);
+			}
+		}
+	}
+}
+
+SDL_Window* createMainWindow(Screen screen){
+	return SDL_CreateWindow(
 		"Super Dash",
 		SDL_WINDOWPOS_CENTERED,
 		SDL_WINDOWPOS_CENTERED,
@@ -44,20 +83,57 @@ int main(){
 		screen.size.h,
 		SDL_WINDOW_RESIZABLE
 	);
-	SDL_Renderer* renderer = SDL_CreateRenderer(
-		mainWindow,
+}
+
+SDL_Renderer* createRenderer(SDL_Window* window){
+	return SDL_CreateRenderer(
+		window,
 		-1,
 		SDL_RENDERER_ACCELERATED
 	);
+}
+
+int main(){
+	Screen screen = setWindowSize(800, 600); //TODO: implement file reading
+	SDL_Init(SDL_INIT_EVERYTHING);
+	SDL_Window* mainWindow = createMainWindow(screen);
+	SDL_Renderer* renderer = createRenderer(mainWindow);
+
+	//player declaration (temporary)
+	Player player;
+	player.pos = {100, 520};
+	player.speed = {0, 0};
+	player.keys = {0, 0, 0, 0};
+	player.state = STATE_WALKING;
+
+	SDL_Texture* bric = IMG_LoadTexture(renderer, "assets/brick.png"); //temporary
 	
-	while(dashSystem.running){
+	while(dashSystem.running){ //almost everything in the loop is temporary
 		//testing
-		eventHandler();
+		eventHandler(player);
+
+		//clear
 		SDL_SetRenderDrawColor(renderer, 0, 0, 0, 1);
 		SDL_RenderClear(renderer);
+
+		//build map
+		buildMap(renderer, bric);
+
+		//move player
+		player.move();
+
+		std::cout << "y: " << player.pos.y << ", ";
+		//std::cout << "sy: " << player.speed.y << ", ";
+		std::cout << "x: " << player.pos.x << ", ";
+		std::cout << "sx: " << player.speed.x << ", ";
+		std::cout << "st: " << player.state << ";\n";
+
+		//draw player
 		SDL_SetRenderDrawColor(renderer, 255, 0, 0, 1);
-		SDL_Rect rectTest = {20, 20, 100, 100};
-		SDL_RenderFillRect(renderer, &rectTest);
+		SDL_Rect charac = {player.pos.x, player.pos.y, 20, 50};
+		SDL_RenderFillRect(renderer, &charac);
+
+		//draw
 		SDL_RenderPresent(renderer);
 		SDL_Delay(1000/120);
 	}
